518830c90bea79fb605687e686bbac9b4d8b62c8
Anders Martini <andersmartini2@gmail.com>
2012-11-09 22:51:46 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var nr1 = "<div><p>välkommen till oss i KronbergsGården nu när barnen vuxit upp och flyttat ut, har vi bestämt oss för att öppna bed and breakfast i deras gamla sovrum som stod tomma. Här kan du för en billig kostnad få uppleva hur det var i konsentrationslägrerna på 40-talet, och vi kommer bli väldigt mätta på er bekostnad! Muhahahahahaha!</p> </div><div><p>dasldasldkasl asldkn asd asdl knasd asldk nasd lasdnasldkn as asdk nasdklnasldknas asldk asldk asldnk asldkn asldnk dnk asldkn asldknas l nasdka</p></div> "
+var nr2 = "<div><p>Det här rummet är mysigt, för här kan du se hela siljan från ditt fönster, och du får en härlig dubbelsäng i gammal stil att sova i!</p></div>"
+var nr3 = "<div><p>Här kan du bo med hela familjen i samma rum! en stor dubbelsäng, och desutom en våningsäng för de små!</p></div>"
+var nr4 = "<div><p>Vi kan ordna med bakstuga, och ni får uppleva det fantastiska Sticher en slaggs tunnbröd med recept som är lokalt för Vikarbyn!!</p></div>"
+
+var hem = document.getElementById("hem").innerHTML
+
+var go = function(x) {
+	document.getElementById('cont').innerHTML = x;
+}
+
